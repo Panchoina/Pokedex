0f80dbcb1c67b6b8b70fe0b784c76ff44973f103
Panchoina <166861882+Panchoina@users.noreply.github.com>
2024-06-15 22:35:43 -0400

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,104 @@
+import { edit, getAll, remove, save, selectOne, subirImagen, mostrarImagenesEnLista } from "./firebase.js"
+
+let id = 0
+
+document.getElementById('btnGuardar').addEventListener('click', async () => {
+    document.querySelectorAll('.we').forEach(item => {
+        verificar(item.id)
+    })   
+    const url = await subirImagen(); // Espera a que se suba la imagen y obtener la URL
+    console.log(url)
+    if (document.querySelectorAll('.is-invalid').length == 0) {
+        const sexo = document.querySelector('input[name="sexo"]:checked');
+        const sexoValue = sexo ? sexo.value : '';
+        const poke = {
+            numero: document.getElementById('numero').value,
+            nom: document.getElementById('nombre').value,
+            tipo: document.getElementById('tipo').value,
+            tipo2: document.getElementById('tipo2').value,
+            sexo: sexoValue, // Asignar el valor del sexo obtenido
+            generacion: document.getElementById('generacion').value,
+            cuando: document.getElementById('cuando').value,
+            url: url}
+    if (document.getElementById('btnGuardar').value == 'Guardar') {
+        save(poke)
+    } else {
+        edit(id, poke)
+        id = 0
+    }limpiar()
+}})
+
+window.addEventListener('DOMContentLoaded', () => {
+    getAll(poke => {
+        let tabla = ''
+        poke.forEach(doc => {
+            const item = doc.data()
+
+            tabla += `<tr>
+                <td>${item.numero}</td>
+                <td>${item.nom}</td>
+                <td>${item.tipo}</td>
+                <td>${item.tipo2}</td>
+                <td>${item.sexo}</td>
+                <td>${item.generacion}</td>
+                <td>${item.cuando}</td>
+                <td><img src="${item.url}" width="50" /></td> <!-- Mostrar imagen -->
+                <td nowrap>
+                    <button class="btn btn-danger" id="${doc.id}">Eliminar</button>
+                    <button class="btn btn-warning" id="${doc.id}">Editar</button>
+                </td>
+            </tr>`
+        })
+        document.getElementById('contenido').innerHTML = tabla
+
+        //eliminar
+        document.querySelectorAll('.btn-danger').forEach(btn => {
+            btn.addEventListener('click', () => {
+                Swal.fire({
+                    title: "¿Estas Seguro Que Quieres Eliminar Tu Registro!!!?",
+                    text: "NO PODRAS REVERTIR LOS CAMBIOS D:",
+                    icon: "error",
+                    showCancelButton: true,
+                    confirmButtonColor: "#d33",
+                    cancelButtonColor: "#3085d6",
+                    confirmButtonText: "Eliminar"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        //eliminar
+                        remove(btn.id)
+                        Swal.fire({
+                            title: "Eliminado!",
+                            text: "Su registro ha sido eliminado con exito.",
+                            icon: "success"
+                        });
+                    }
+                });
+            })
+        })
+
+        // boton editar
+        document.querySelectorAll('.btn-warning').forEach(btn => {
+            btn.addEventListener('click', async () => {
+                const pok = await selectOne(btn.id)
+                const item = pok.data()
+                //nuevos valores
+                document.getElementById('numero').value = item.numero
+                document.getElementById('nombre').value = item.nom
+                document.getElementById('tipo').value = item.tipo
+                document.getElementById('tipo2').value = item.tipo2
+                document.querySelector(`input[name="sexo"][value="${item.sexo}"]`).checked = true;
+                document.getElementById('generacion').value = item.generacion
+                document.getElementById('cuando').value = item.cuando
+                //cambiar el boton de guardar a editar
+                document.getElementById('btnGuardar').value = 'Editar'
+                id = btn.id
+            })
+        })
+
+    })
+})
+
+// boton limpiar
+document.getElementById('limpiar').addEventListener('click', () => {
+    limpiar()
+})
---
+++ helper.js
@@ -0,0 +1,64 @@
+const verificar = async (id) => {
+    const input = document.getElementById(id)
+    const div = document.getElementById('e-' + id)
+    input.classList.remove('is-invalid')
+    if (input.value.trim() == '') {
+        input.classList.add('is-invalid')
+        div.innerHTML = '<span class="badge bg-danger">El campo es obligatorio</span>'
+    }
+    else {
+        const existe = await verificarNumeroExistente(input.value);
+        if (existe) {
+            input.classList.add('is-invalid');
+            div.innerHTML = '<span class="badge bg-danger">El número ya está en uso</span>';
+        }
+        if (id == 'numero'){
+            div.innerHTML = ''
+            if (input.value > 42) {
+                input.classList.add('is-invalid')
+                div.innerHTML = '<span class="badge bg-danger">Porfavor Ingrese Un ID De El 1 Al 42</span>'
+            }
+        }
+        if (id == 'cuando') {
+            if (validarFecha(input.value) < 1) {
+                input.classList.add('is-invalid')
+                div.innerHTML = '<span class="badge bg-danger">No Puede Ingresar Pensando A Futuro</span>'
+            }
+        }
+    }
+}
+const validarFecha = (fecha) => {
+    const hoy = new Date()
+    fecha = new Date(fecha)
+    const resta = hoy - fecha
+    const dia = (resta / (1000 * 60 * 60 * 24))
+    return dia.toFixed(0)
+}
+const limpiar = () => {
+    document.querySelector('form').reset()
+    document.querySelectorAll('.form-control').forEach(item => {
+        item.classList.remove('is-invalid')
+        item.classList.remove('is-valid')
+    })
+    document.querySelectorAll('[id^="e-"]').forEach(div => {
+        div.innerHTML = '';
+    });
+}
+const soloNumero = (evt) => {
+    if (evt.keyCode >= 48 && evt.keyCode <= 57)
+        return true
+    return false
+}
+const verificarNumeroExistente = async (num) => {
+    return new Promise((resolve, reject) => {
+        getAll(item => {
+            let existe = false;
+            item = doc.data();
+            if (item.numero == num) {
+                existe = true;
+            }
+            else{
+            existe = false;}
+        });
+    });
+}
